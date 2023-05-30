bced51f44c4b856072ed0e31dd0c2f10d3526161
Arsene Fokam P <2210100@isi-montreal.com>
2023-05-30 12:30:53 -0400

    commit test

--- ISIDrone/src/manager/MDB.java
+++ ISIDrone/src/manager/MDB.java
@@ -1,8 +1,6 @@
 package manager;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,45 +9,41 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import sun.java2d.pipe.RenderingEngine;
+
 
 public class MDB {
 	private static Connection connection = null;
         private static InputStream inputStream;
-        private static 
+        private static String dbName, dbIp,dbUserName, dbPassword;
 
         public static void feedDbProterties()  {
         try {
             Properties prop = new Properties();
             String propFileName = "configs.properties";
-            inputStream = ClassLoader.class.getResourceAsStream(propFileName);
+            inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(propFileName);
             if (inputStream != null) {
                 prop.load(inputStream);
             } else {
                 throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
             }
             // get the property value and print it out
-            String DB_NAME = prop.getProperty("DB_NAME");
-            String DB_IP = prop.getProperty("DB_IP");
-            String DB_USERNAME = prop.getProperty("DB_USERNAME");
-            String DB_PASSWORD = prop.getProperty("DB_PASSWORD");
-            String result = "DB PROP = " + DB_NAME + ", " + DB_IP + ", " + DB_USERNAME + ", " + DB_PASSWORD;
-            System.out.println(result );
+             dbName = prop.getProperty("DB_NAME");
+             dbIp = prop.getProperty("DB_IP");
+             dbUserName = prop.getProperty("DB_USERNAME");
+             dbPassword = prop.getProperty("DB_PASSWORD");
+           
         } catch (Exception e) {
             System.out.println("Exception: " + e);
         } 
         }
 	
 	public static void connect() throws SQLException {
-                feedDbProterties();
-               System.out.println(new File(".").getAbsolutePath());
-
+          //     feedDbProterties();
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			String mysqlURL="jdbc:mysql://127.0.0.1:3306/isidrone?serverTimezone=UTC";
-			connection = DriverManager.getConnection(mysqlURL, "root", "");
+                        String mysqlURL2=String.format("jdbc:mysql://%s:3306/%s?serverTimezone=UTC", dbIp, dbName);
+                         System.out.println(mysqlURL2);
+			connection = DriverManager.getConnection(mysqlURL2, dbUserName, dbPassword);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
